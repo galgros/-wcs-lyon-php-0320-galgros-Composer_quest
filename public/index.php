62aa4e792d867d6c2e9beffc3088498abbcfc486
galgros <galgros@yahoo.fr>
2020-04-07 14:06:57 +0200

    create repo and add files

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$hello = new \App\Wcs\Hello();
+
+echo $hello->talk();
---
+++ src/wcs/Hello.php
@@ -0,0 +1,12 @@
+<?php namespace App\Wcs;
+
+class Hello
+{
+
+    const SENTENCE = "Hello World!";
+
+    public function talk() : string
+    {
+        return self::SENTENCE;
+    }
+}
